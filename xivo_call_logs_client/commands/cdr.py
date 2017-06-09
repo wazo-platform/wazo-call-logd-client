0d8d701927664fd49acb009fbea269511cfd4780
Sebastien Duthil <sduthil@wazo.community>
2017-06-09 11:24:28 -0400

    cdr: add CSV methods

--- xivo_call_logs_client/commands/cdr.py
+++ xivo_call_logs_client/commands/cdr.py
@@ -14,6 +14,14 @@ class CDRCommand(CallLogdCommand):
         self.raise_from_response(r)
         return r.json()
 
+    def list_csv(self, **params):
+        if 'from_' in params:
+            params['from'] = params.pop('from_')
+
+        r = self.session.get(self._client.url('cdr'), params=params, headers={'Accept': 'text/csv; charset=utf-8'})
+        self.raise_from_response(r)
+        return r.text
+
     def list_for_user(self, user_uuid, **params):
         if 'from_' in params:
             params['from'] = params.pop('from_')
@@ -22,6 +30,14 @@ class CDRCommand(CallLogdCommand):
         self.raise_from_response(r)
         return r.json()
 
+    def list_for_user_csv(self, user_uuid, **params):
+        if 'from_' in params:
+            params['from'] = params.pop('from_')
+
+        r = self.session.get(self._client.url('users', user_uuid, 'cdr'), params=params, headers={'Accept': 'text/csv; charset=utf-8'})
+        self.raise_from_response(r)
+        return r.text
+
     def list_from_user(self, **params):
         if 'from_' in params:
             params['from'] = params.pop('from_')
@@ -29,3 +45,11 @@ class CDRCommand(CallLogdCommand):
         r = self.session.get(self._client.url('users', 'me', 'cdr'), params=params)
         self.raise_from_response(r)
         return r.json()
+
+    def list_from_user_csv(self, **params):
+        if 'from_' in params:
+            params['from'] = params.pop('from_')
+
+        r = self.session.get(self._client.url('users', 'me', 'cdr'), params=params, headers={'Accept': 'text/csv; charset=utf-8'})
+        self.raise_from_response(r)
+        return r.text
