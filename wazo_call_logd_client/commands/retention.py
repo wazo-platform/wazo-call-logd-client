40b60f6212e2b2d5cf963c9fa45ddc21fe6246b1
Francois Blackburn <fblackburn@wazo.community>
2021-04-30 10:55:03 -0400

    send Content-Type header on update

--- wazo_call_logd_client/commands/retention.py
+++ wazo_call_logd_client/commands/retention.py
@@ -2,30 +2,20 @@
 # Copyright 2021 The Wazo Authors  (see AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-from wazo_call_logd_client.command import CallLogdCommand
+from .helpers.base import BaseCommand
 
 
-class RetentionCommand(CallLogdCommand):
-
-    headers = {'Accept': 'application/json'}
+class RetentionCommand(BaseCommand):
 
     def get(self, tenant_uuid=None):
-        tenant_uuid = tenant_uuid or self._client.tenant()
-        headers = self.headers.copy()
-        if tenant_uuid:
-            headers['Wazo-Tenant'] = tenant_uuid
-
+        headers = self._get_headers(tenant_uuid=tenant_uuid)
         url = self._client.url('retention')
         r = self.session.get(url, headers=headers)
         self.raise_from_response(r)
         return r.json()
 
     def update(self, tenant_uuid=None, **body):
-        tenant_uuid = tenant_uuid or self._client.tenant()
-        headers = self.headers.copy()
-        if tenant_uuid:
-            headers['Wazo-Tenant'] = tenant_uuid
-
+        headers = self._get_headers(write=True, tenant_uuid=tenant_uuid)
         url = self._client.url('retention')
         r = self.session.put(url, json=body, headers=headers)
         self.raise_from_response(r)
