aab239be9d9ea8dcbef179924beb469ed266fad6
Francois Blackburn <fblackburn@wazo.community>
2021-04-30 15:27:02 -0400

    fix Copyright typo

--- wazo_call_logd_client/__init__.py
+++ wazo_call_logd_client/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2017 The Wazo Authors  (see AUTHORS file)
+# Copyright 2017 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from wazo_call_logd_client.client import Client
--- wazo_call_logd_client/exceptions.py
+++ wazo_call_logd_client/exceptions.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2017 The Wazo Authors  (see AUTHORS file)
+# Copyright 2017 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from requests import HTTPError
