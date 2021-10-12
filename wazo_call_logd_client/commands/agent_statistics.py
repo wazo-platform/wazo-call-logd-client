5ce2b50edfdc93483f4c2b42eed705202e20b72c
Francois Blackburn <fblackburn@wazo.community>
2021-10-12 14:09:59 -0400

    refactor method to use get_headers
    note: get_headers from RESTCommand is not used because BaseCommand
    inherit from HTTPCommand and I don't want to make a bigger change

--- wazo_call_logd_client/commands/agent_statistics.py
+++ wazo_call_logd_client/commands/agent_statistics.py
@@ -1,17 +1,19 @@
 # -*- coding: utf-8 -*-
-# Copyright 2020 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2020-2021 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-from wazo_call_logd_client.command import CallLogdCommand
+from .helpers.base import BaseCommand
 
 
-class AgentStatisticsCommand(CallLogdCommand):
+class AgentStatisticsCommand(BaseCommand):
 
     def get_by_id(self, agent_id, **params):
         if 'from_' in params:
             params['from'] = params.pop('from_')
 
-        r = self.session.get(self._client.url('agents', agent_id, 'statistics'), params=params)
+        headers = self._get_headers()
+        url = self._client.url('agents', agent_id, 'statistics')
+        r = self.session.get(url, params=params, headers=headers)
         self.raise_from_response(r)
         return r.json()
 
@@ -19,6 +21,8 @@ class AgentStatisticsCommand(CallLogdCommand):
         if 'from_' in params:
             params['from'] = params.pop('from_')
 
-        r = self.session.get(self._client.url('agents', 'statistics'), params=params)
+        headers = self._get_headers()
+        url = self._client.url('agents', 'statistics')
+        r = self.session.get(url, params=params, headers=headers)
         self.raise_from_response(r)
         return r.json()
--- wazo_call_logd_client/commands/cdr.py
+++ wazo_call_logd_client/commands/cdr.py
@@ -2,14 +2,15 @@
 # Copyright 2017-2021 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-from wazo_call_logd_client.command import CallLogdCommand
+from .helpers.base import BaseCommand
 
 
-class CDRCommand(CallLogdCommand):
+class CDRCommand(BaseCommand):
 
     def get_by_id(self, cdr_id):
+        headers = self._get_headers()
         url = self._client.url('cdr', cdr_id)
-        r = self.session.get(url)
+        r = self.session.get(url, headers=headers)
         self.raise_from_response(r)
         return r.json()
 
@@ -24,8 +25,9 @@ class CDRCommand(CallLogdCommand):
         if 'from_' in params:
             params['from'] = params.pop('from_')
 
+        headers = self._get_headers()
         url = self._client.url('cdr')
-        r = self.session.get(url, params=params)
+        r = self.session.get(url, params=params, headers=headers)
         self.raise_from_response(r)
         return r.json()
 
@@ -43,8 +45,9 @@ class CDRCommand(CallLogdCommand):
         if 'from_' in params:
             params['from'] = params.pop('from_')
 
+        headers = self._get_headers()
         url = self._client.url('users', user_uuid, 'cdr')
-        r = self.session.get(url, params=params)
+        r = self.session.get(url, params=params, headers=headers)
         self.raise_from_response(r)
         return r.json()
 
@@ -62,8 +65,9 @@ class CDRCommand(CallLogdCommand):
         if 'from_' in params:
             params['from'] = params.pop('from_')
 
+        headers = self._get_headers()
         url = self._client.url('users', 'me', 'cdr')
-        r = self.session.get(url, params=params)
+        r = self.session.get(url, params=params, headers=headers)
         self.raise_from_response(r)
         return r.json()
 
@@ -78,30 +82,22 @@ class CDRCommand(CallLogdCommand):
         return r.text
 
     def delete_cdrs_recording_media(self, cdr_ids, **kwargs):
-        tenant_uuid = kwargs.pop('tenant_uuid', None) or self._client.tenant()
-        headers = {}
-        if tenant_uuid:
-            headers['Wazo-Tenant'] = tenant_uuid
+        headers = self._get_headers(**kwargs)
         url = self._client.url('cdr', 'recordings', 'media')
         body = {'cdr_ids': cdr_ids}
         r = self.session.delete(url, json=body, headers=headers)
         self.raise_from_response(r)
 
     def get_recording_media(self, cdr_id, recording_uuid, **kwargs):
-        tenant_uuid = kwargs.pop('tenant_uuid', None) or self._client.tenant()
-        headers = {'Accept': '*/*'}
-        if tenant_uuid:
-            headers['Wazo-Tenant'] = tenant_uuid
+        headers = self._get_headers(**kwargs)
+        headers['Accept'] = '*/*'
         url = self._client.url('cdr', cdr_id, 'recordings', recording_uuid, 'media')
         r = self.session.get(url, headers=headers)
         self.raise_from_response(r)
         return r
 
     def delete_recording_media(self, cdr_id, recording_uuid, **kwargs):
-        tenant_uuid = kwargs.pop('tenant_uuid', None) or self._client.tenant()
-        headers = {}
-        if tenant_uuid:
-            headers['Wazo-Tenant'] = tenant_uuid
+        headers = self._get_headers(**kwargs)
         url = self._client.url('cdr', cdr_id, 'recordings', recording_uuid, 'media')
         r = self.session.delete(url, headers=headers)
         self.raise_from_response(r)
@@ -109,10 +105,7 @@ class CDRCommand(CallLogdCommand):
     def export_recording_media(self, cdr_ids=None, **params):
         if 'from_' in params:
             params['from'] = params.pop('from_')
-        tenant_uuid = params.pop('tenant_uuid', None) or self._client.tenant()
-        headers = {}
-        if tenant_uuid:
-            headers['Wazo-Tenant'] = tenant_uuid
+        headers = self._get_headers(**params)
         body = {}
         if cdr_ids:
             body['cdr_ids'] = cdr_ids
